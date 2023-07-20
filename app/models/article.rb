02214a2ffe1108e9049a71e8e5d47bf1c280e423
yanase <tennisman_225@yahoo.co.jp>
2023-07-20 22:18:43 +0900

    annotate, better_errors, binding_of_caller, haml, pry-byebug, rubocopを導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,18 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :content, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
 
-    def display_created_at
-        I18n.l(self.created_at, format: :default)
-    end
+  def display_created_at
+    I18n.l(created_at, format: :default)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_11_001458) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_230_711_001_458) do
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.text 'content'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end

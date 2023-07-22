b2843e05b6d0fd688d73f45ffbb806f61baa370d
yanase <tennisman_225@yahoo.co.jp>
2023-07-22 16:07:12 +0900

    deviseを導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
-  validates :title, format: { with: /\A(?!\@)/ }
+  validates :title, format: { with: /\A(?!@)/ }
 
   validates :content, presence: true
   validates :content, length: { minimum: 10 }
@@ -24,8 +24,9 @@ class Article < ApplicationRecord
   end
 
   private
+
   def validate_title_and_content_length
-    char_count = self.title.length + self.content.length
-      errors.add(:content, '100文字いじょうで！') unless char_count > 100
+    char_count = title.length + content.length
+    errors.add(:content, '100文字いじょうで！') unless char_count > 100
   end
 end

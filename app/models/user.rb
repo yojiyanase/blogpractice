4ed2f7ba3782f6a91c78a39348086e73ccccebb1
yanase <tennisman_225@yahoo.co.jp>
2023-10-10 22:47:06 +0900

    フォローできるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,10 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
+
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
@@ -41,7 +45,11 @@ class User < ApplicationRecord
     profile&.nickname || self.email.split('@').first
   end
 
-  def prepare_profile
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+    def prepare_profile
     profile || build_profile
   end
 
